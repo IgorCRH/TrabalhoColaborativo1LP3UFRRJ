993d5b3982c8139f84ff3d742c2d8bfcb916a40b
IgorCRH <125159948+IgorCRH@users.noreply.github.com>
2023-05-23 15:54:19 -0300

    Add files via upload

--- Problema1_LP3/src/Teste.java
+++ Problema1_LP3/src/Teste.java
@@ -1,19 +1,44 @@
+import java.util.Scanner;
 
-public class Teste 
-{
-	public static void main(String[] args) 
-	{
-		Navio[] meusNavios = new Navio[5];
-		
-		meusNavios[0] = new Cargueiro("amanda","1984",300);
-		meusNavios[1] = new Cargueiro("jessica","2000",450);
-		meusNavios[2] = new Cruzeiro("Princesa do Mar","2013",1500);
-		meusNavios[3] = new Cruzeiro("Camarao Vermelho","1950",3050);
-		meusNavios[4] = new Navio("Juliano","2022");
-		
-		for(int i=0;i<5;i++)
-		{
-			System.out.println(meusNavios[i].toString());
-		}
-	}
+public class Teste {
+    public static void main(String[] args) {
+        Navio[] meusNavios = new Navio[5];
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Digite as informações do navio " + (i + 1) + ":");
+
+            System.out.print("Nome: ");
+            String nome = scanner.nextLine();
+
+            System.out.print("Ano de fabricação: ");
+            String anoFabricacao = scanner.nextLine();
+
+            System.out.print("Tipo (Cargueiro ou Cruzeiro): ");
+            String tipo = scanner.nextLine();
+
+            if (tipo.equalsIgnoreCase("Cargueiro")) {
+                System.out.print("Capacidade de carga (em toneladas): ");
+                int capacidadeCarga = scanner.nextInt();
+                scanner.nextLine(); // Consumir a quebra de linha
+
+                meusNavios[i] = new Cargueiro(nome, anoFabricacao, capacidadeCarga);
+            } else if (tipo.equalsIgnoreCase("Cruzeiro")) {
+                System.out.print("Número máximo de passageiros: ");
+                int maxPassageiros = scanner.nextInt();
+                scanner.nextLine(); // Consumir a quebra de linha
+
+                meusNavios[i] = new Cruzeiro(nome, anoFabricacao, maxPassageiros);
+            } else {
+                System.out.println("Tipo inválido. O navio será considerado comum.");
+                meusNavios[i] = new Navio(nome, anoFabricacao);
+            }
+        }
+
+        scanner.close();
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(meusNavios[i].toString());
+        }
+    }
 }
